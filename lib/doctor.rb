51278d1170e02bfd65f788eb557456141949db71
Devone Austin <devone.austin@gmail.com>
2020-04-29 17:03:32 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,7 +15,11 @@ class Doctor
   end
 
   def new_appointment date, patient
-    Appointment.new(date, self, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| patient.appointment == self  }
   end
 
 end
